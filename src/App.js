e74541422c851f22099a6355a7729c4a23ddbde2
Oluwasegun <adeolusegun99@gmail.com>
2022-09-28 03:37:32 +0100

    bootstrapped my project and added the headless ui dependencies I want to switch between posts and users

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Tab } from "@headlessui/react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-3xl font-bold underline text-red-800 my-10">
+        Welcome To The New Era
+      </h1>
+      <Tab.Group>
+        <Tab.List className="flex justify-center">
+          <Tab
+            className={({ selected }) =>
+              `whitespace-nowrap rounded-t-lg py-3 px-3 text-sm font-light
+                  outline-none md:py-4 md:px-6 md:text-base ${
+                    selected
+                      ? "borderGradient bg-[#18615b] text-[#120302]"
+                      : "border-b-2 border-[#120302] text-[#d4af37]"
+                  }
+                  `
+            }
+          >
+            Posts
+          </Tab>
+          <Tab
+            className={({ selected }) =>
+              `whitespace-nowrap rounded-t-lg py-3 px-3 text-sm font-light
+                  outline-none md:py-4 md:px-6 md:text-base ${
+                    selected
+                      ? "borderGradient bg-[#18615b] text-[#120302]"
+                      : "border-b-2 border-[#120302] text-[#d4af37]"
+                  }
+                  `
+            }
+          >
+            Users
+          </Tab>
+          <Tab
+            className={({ selected }) =>
+              `whitespace-nowrap rounded-t-lg py-3 px-3 text-sm font-light
+                  outline-none md:py-4 md:px-6 md:text-base ${
+                    selected
+                      ? "borderGradient bg-[#18615b] text-[#120302]"
+                      : "border-b-2 border-[#120302] text-[#d4af37]"
+                  }
+                  `
+            }
+          >
+            Tab 3
+          </Tab>
+        </Tab.List>
+        <Tab.Panels className="mx-auto pt-15 pb-25 max-w-fit sm:px-4">
+          <Tab.Panel className="tabPanel">Content 1</Tab.Panel>
+          <Tab.Panel className="tabPanel">Content 2</Tab.Panel>
+          <Tab.Panel className="tabPanel">Content 3</Tab.Panel>
+        </Tab.Panels>
+      </Tab.Group>
     </div>
   );
 }
